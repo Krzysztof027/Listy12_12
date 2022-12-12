e74445b77b1d638398ec93cac956408b96234207
Krzysztof028 <krzysztofszczerkowski525266@gmail.com>
2022-12-12 23:55:54 +0100

    Projekt lekcja 8 12 2022 J3

--- Lista/MainWindow.xaml.cs
+++ Lista/MainWindow.xaml.cs
@@ -21,12 +21,28 @@ namespace Lista
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        public IEnumerable<string> Slowa { get; set; }
+        //Aby móc korzystać z pętli foreach musimy na naszej kolekcji zaimplementować interfejs IEnumerable. 
+        public ObservableCollection<pizza> WszystkiePizze { get; set; } //samo będzie się aktualizować
+        //Reprezentuje dynamiczną kolekcję danych, która dostarcza powiadomienia po dodaniu lub usunięciu elementów albo odświeżeniu całej listy.
         public MainWindow()
         {
             InitializeComponent();
 
-            
+            Slowa = new List<string>();
+            //split-rodziel
+            Slowa = "Ala ma kota,a kot ma Ale, Ala ma kota,a kot ma Ale, Ala ma kota,a kot ma Ale".Split(' ').ToList();//Wrzucenie do listy
+            DataContext = this;//Inne Bindowanie
+            przygotujDane();
+        }
+        private void przygotujDane()
+        {
+            //Wpiew żeby wywołać w metodzie w tym przypadku "pizze" musimy stworzyć odpowiedni konstruktor w klasie
+            WszystkiePizze = new ObservableCollection<pizza>();
+            WszystkiePizze.Add(new pizza("Hawajska", 25, "szynka, ser mozarella, ananas z puszki czy sos pomidorowy", true));//true- bo mamy typ bool (prawda czy fałsz), albo true czy false
+            WszystkiePizze.Add(new pizza("Morska", 30, " 40 gr ciasta, 2 puszki pomidorów bez skórki, 2 łyżki oregano, 2 ząbki czosnku, oliwa z oliwek", false));
+            WszystkiePizze.Add(new pizza("Carbonara", 28, "Mąka przenna, woda, parmezan, jogurt grecki", true));
+            WszystkiePizze.Add(new pizza("Mięsna Uczta", 31, "Mięso, pieczarki, ser, oliwki", true));
         }
     }
 }
--- Lista/pizza.cs
+++ Lista/pizza.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Lista.MainWindow;
+using System.Windows;
 
 namespace Lista
 {
-    public class pizza
+    public partial class MainWindow : Window
     {
-        
+
+        public class pizza
+        {
+            public string Nazwa { get; set; }
+            public int Cena { get; set; }
+            public string Skladniki { get; set; }
+            public bool czyCienka { get; set; }
+
+            public pizza(string nazwa, int cena, string skladniki, bool czyCienka)
+            {
+                Nazwa = nazwa;
+                Cena = cena;
+                Skladniki = skladniki;
+                this.czyCienka = czyCienka;
+            }
+        }
     }
 }
+
